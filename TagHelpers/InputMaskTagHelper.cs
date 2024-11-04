87fb060d497192c25ac98d6bc175ae0be84f387c
Fabián Medrano <135065707+fabianmedrano@users.noreply.github.com>
2024-11-04 08:24:28 -0600

    no recuerdo pero estos son los ultimos cambios antes de hacer cambios en el theme

---
+++ TagHelpers/InputMaskTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace subscription_system.TagHelpers {
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("input", Attributes ="asp-mask")]
+    public class InputMaskTagHelper : TagHelper {
+
+
+        public string AspMask { set; get; } = "";
+        public override void Process(TagHelperContext context, TagHelperOutput output) {
+            if (output.TagName == "input") output.Attributes.SetAttribute("Data-mask", AspMask);
+
+            if (AspMask == "currency") {
+                output.Attributes.SetAttribute("data-prefix", "$");
+                output.Attributes.SetAttribute("data-group-separator", ",");
+                output.Attributes.SetAttribute("data-digits", "2");
+                output.Attributes.SetAttribute("data-right-align", "false");
+            }
+        }
+    }
+}
---
+++ wwwroot/js/mask.js
@@ -0,0 +1,36 @@
+﻿
+$(function () {
+
+    $('input[data-mask]').each(function () {
+        var mask = $(this).data('mask');
+
+        if (mask === 'currency') {
+            $(this).inputmask({
+                alias: "currency",
+                prefix: $(this).data('prefix'),
+                groupSeparator: $(this).data('group-separator'),
+                autoGroup: true,
+                digits: $(this).data('digits'),
+                digitsOptional: false,
+                placeholder: "0.00",
+                rightAlign: $(this).data('right-align')
+            });
+        } else if (mask === 'months') {
+            console.log("here")
+            $(this).inputmask({
+                alias: "numeric",
+                min: 1,
+                max: 12,
+                placeholder: "MM",
+                digits: 0,
+                rightAlign: false,
+                autoGroup: true,
+                allowMinus: false,
+                allowPlus: false,
+            });
+        }else {
+            $(this).inputmask(mask); // Aplica cualquier otra máscara
+        }
+    });
+
+});
