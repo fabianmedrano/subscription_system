79c879d5807477c140fc7eba980e6af5c53fb387
Fabián Medrano <135065707+fabianmedrano@users.noreply.github.com>
2025-02-20 07:55:31 -0600

    cambio template

--- wwwroot/js/mask.js
+++ wwwroot/js/mask.js
@@ -18,17 +18,17 @@ $(function () {
         } else if (mask === 'months') {
             console.log("here")
             $(this).inputmask({
-                alias: "numeric",
-                min: 1,
-                max: 12,
-                placeholder: "MM",
-                digits: 0,
-                rightAlign: false,
+                alias: "currency",
+                prefix: $(this).data('prefix'),
+                groupSeparator: $(this).data('group-separator'),
                 autoGroup: true,
-                allowMinus: false,
-                allowPlus: false,
+                digits: $(this).data('digits'),
+                digitsOptional: false,
+                placeholder: "0.00",
+                rightAlign: $(this).data('right-align')
             });
-        }else {
+        }
+        else {
             $(this).inputmask(mask); // Aplica cualquier otra máscara
         }
     });
